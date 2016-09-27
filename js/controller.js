2ca115fac9f2a0659df3554ea2f88b232f96e374
bmanning1 <rebm01@hotmail.co.uk>
2016-09-27 18:14:03 +0100

    Commit 1

---
+++ js/controller.js
@@ -0,0 +1,16 @@
+angular.module('RouteControllers', [])
+    .controller('MeetTheBandController', function($scope) {
+    
+    })
+    .controller('ListenController', function($scope) {
+    
+    })
+    .controller('BookingsController', function($scope) {
+    
+    })
+    .controller('ContactController', function($scope) {
+    
+    })
+    .controller('HomeController', function($scope) {
+    
+    });
---
+++ js/jQuery.js
@@ -0,0 +1,5 @@
+$(document).ready(function {
+          $('a').hover(function {
+                  $(this).css('background-color', 'blue')  
+            });
+});
